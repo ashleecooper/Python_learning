aada9ffd4cf290e9b6330c866416e749907d8268
Ashlee Cooper <ashleedolan1@yahoo.com>
2022-09-05 04:03:24 +0000

    create_dynamodb_table

--- learn_lists.py
+++ learn_lists.py
@@ -1,7 +1,7 @@
 empty_list = []
 print(empty_list)
 
-numbers = [2, 4, 6, 4]
+some_numbers = [2, 4, 6, 4]
 print(numbers)
 
 number_series = [1, 2, 3, 4, 5]
@@ -11,3 +11,5 @@ number_series_range = list(range(0,100))
 number_series_range = number_series_range + [101]
 number_series_range.append(102)
 print(number_series_range)
+
+
